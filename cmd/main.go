6d124d28ac0b2c70804a02e06615d42e893ae1ec
aminjonshermatov <aminjonshermatov@gmail.com>
2021-04-16 22:19:31 +0500

    PayFromFavorite()

--- cmd/main.go
+++ cmd/main.go
@@ -3,29 +3,24 @@ package main
 import (
 	"fmt"
 	"github.com/aminjonshermatov/wallet/pkg/wallet"
+	"github.com/google/uuid"
 )
 
 func main() {
 	svc := &wallet.Service{}
 
 	account, err := svc.RegisterAccount("+992000000001")
-
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
 	err = svc.Deposit(account.ID, 10)
-
 	if err != nil {
-		switch err {
-		case wallet.ErrAmountMustBePositive:
-			fmt.Println("Amount must be greater then 0")
-		case wallet.ErrAccountNotFound:
-			fmt.Println("Account not found")
-		}
 		return
 	}
 
-	fmt.Println(account.Balance) // 10
+	payment, err := svc.PayFromFavorite(uuid.New().String())
+	id := payment.ID
+	fmt.Println(id)
 }
